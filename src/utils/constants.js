c31d3c9052556f222f7d4914cec352303d761a97
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2023-12-13 22:49:02 +0530

    implemented secondary container

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const API_OPTIONS = {
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZWI5ZDAyMDRiMGNmZDAzZTBiODIyOTg2NzliOTI5NiIsInN1YiI6IjY0ZWY2MDAzZTBjYTdmMDE0ZjY5YjZjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9nNJCcfkvsLzIv3jI5STQmXMMnyKILbSfGAp4Ew--Wc',
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
